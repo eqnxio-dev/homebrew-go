66c4faf974376df483d01c2dbbe8f7114dae26bb
Kyle Conroy <kyle@kyleconroy.com>
2018-10-23 12:21:39 -0700

    Publish github-com-kyleconroy-hellogopher 3b9c277bba3f9f0185c2465815890e3a7d2f68e7

---
+++ Formula/github-com-kyleconroy-hellogopher.rb
@@ -0,0 +1,12 @@
+class GithubComKyleconroyHellogopher < Formula
+  desc ''
+  homepage 'https://go.equionx.io/github.com/kyleconroy/hellogopher'
+
+  url 'http://bin.equinox.test:8000/a/iApDK6yYT4G/github-com-kyleconroy-hellogopher-3b9c277bba3f9f0185c2465815890e3a7d2f68e7-darwin-amd64.tar.gz'
+  sha256 'c72765e741811124223724f8aa91ceefe84962ff3aa8e7ee9a5782d8b5437717'
+  version '3b9c277bba3f9f0185c2465815890e3a7d2f68e7'
+
+  def install
+    bin.install 'hellogopher'
+  end
+end
